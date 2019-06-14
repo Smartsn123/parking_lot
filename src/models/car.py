7f84c31b48e06ac28d1df826158927b4edc83d02
B0208131 <sunny.singh@airtel.com>
2019-06-14 22:01:34 +0530

    first base model calsses

---
+++ src/models/car.py
@@ -0,0 +1,32 @@
+
+class Car:
+    '''
+    Class to represent a car object
+    '''
+
+    def __int__(self, reg_no, color):
+        '''
+        Constructor for Car class variables
+        :param reg_no: registration no of the car
+        :param color: color of the car
+        :return: returns car object by default
+        Complexity : O(1)
+        '''
+        self._reg_no = reg_no
+        self._color = color
+
+    @property
+    def reg_no(self):
+        '''
+        :return: property getter for car's registration no
+        Complexity : O(1)
+        '''
+        return self._reg_no
+
+    @property
+    def car_color(self):
+        '''
+        :return: property getter for car's color
+        Complexity : O(1)
+        '''
+        return self._color
